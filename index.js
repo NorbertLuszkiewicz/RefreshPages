4533899d55001dab1232cc562d169d2362d192c8
Norbert Luszkiewicz <norbert.luszkiewicz@stxnext.pl>
2023-05-03 08:29:05 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,31 @@
+const http = require("http");
+const fetch = require("node-fetch");
+
+const hostname = "127.0.0.1";
+const port = 3000;
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader("Content-Type", "text/plain");
+  res.end("Hello World");
+});
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
+
+const pagesToRefresh = ["https://dynamix-bot.glitch.me"];
+const refreshTime = 3 * 60 * 1000;
+
+const refreshPages = urls => {
+  urls.forEach(url => {
+    sendRequest(url);
+    setInterval(() => sendRequest(url), refreshTime);
+  });
+};
+
+const sendRequest = url => {
+  fetch(url).then(res => console.log(res, "dd"));
+};
+
+refreshPages(pagesToRefresh);
